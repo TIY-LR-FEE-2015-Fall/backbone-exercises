3a67a28bc5922668738adc175b82043d0450a9a4
Ryan Tablada <ryan.tablada@gmail.com>
2015-11-25 07:08:33 -0600

    User can submit contact

--- src/views/person-form.js
+++ src/views/person-form.js
@@ -1,9 +1,45 @@
+// Grab the PersonModel
+import PersonModel from '../models/person';
+
 export default Backbone.View.extend({
+  firstNameInput: null,
+  lastNameInput: null,
+  addressInput: null,
+  phoneInput: null,
+
+  initialize() {
+    this.firstNameInput = this.$el.find('#person-form__first-name');
+    this.lastNameInput = this.$el.find('#person-form__last-name');
+    this.addressInput = this.$el.find('#person-form__address');
+    this.phoneInput = this.$el.find('#person-form__phone');
+  },
+
+  clearForm() {
+    this.firstNameInput.val('');
+    this.lastNameInput.val('');
+    this.addressInput.val('');
+    this.phoneInput.val('');
+  },
+
   // Listen for events
   events: {
     // Form to Submit
     submit() {
-      debugger;
+      // Get User Input
+      var firstName = this.firstNameInput.val();
+      var lastName = this.lastNameInput.val();
+      var address = this.addressInput.val();
+      var phone = this.phoneInput.val();
+
+      // Create a new Person instance
+      var person = new PersonModel({firstName, lastName, address, phone});
+
+      // Save it to the server
+      person.save().then(() => {
+        alert('Contact has been saved');
+
+        this.clearForm();
+      });
     },
   },
 });
